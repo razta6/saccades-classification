5f35c2770ed7d44e97f61015e86dd0e75af3c01c
razta6 <razta6@gmail.com>
2018-03-03 14:49:22 +0200

    Signed-off-by: razta6 <razta6@gmail.com>

--- datasets/pixel_data.py
+++ datasets/pixel_data.py
@@ -1,7 +1,8 @@
 import numpy as np
 from experiment_parser import *
 
-def create_dataset(trials, binary=True):
+
+def create_dataset(trials, label_set, binary=True):
     num_trials = len(trials)
     X = []
     Y = []
@@ -12,30 +13,19 @@ def create_dataset(trials, binary=True):
             print(i+1)
         trial = trials[i].to_array()
         X.append(trial.flatten())
-        #Y.append(trials[i].stim_type_ind*10)
+        #Y.append(trials[i].stim_type_ind)
         Y.append(trials[i].get_bid(bid_type='binary'))
     
     print('Done')
     print()
     
     X, Y = np.array(X), np.array(Y)
-    '''
-    Y[Y==(1*10)] = 0
-    Y[Y==(2*10)] = 1
-    Y[Y==(3*10)] = 2
-    Y[Y==(6*10)] = 3
     
-    if binary:
-        Y[Y==(0)] = 10
-        Y[Y==(1)] = 11
-        Y[Y==(2)] = 12
-        Y[Y==(3)] = 13
-
-        Y[Y==(10)] = 0
-        Y[Y==(11)] = 1
+    for i in range(len(label_set)):
+        Y[Y==(label_set[i])] = i
         
-        idx = np.where(Y<2)
-        X = X[idx]
-        Y = Y[idx]
-    ''' 
+    idx = np.where(Y<len(label_set))
+    X = X[idx]
+    Y = Y[idx]
+
     return X, Y
--- run_experiment.py
+++ run_experiment.py
@@ -18,6 +18,9 @@ DATA_FOLDER2 = EXP_NAME2 + '_data'
 PICKLE_FILE = EXP_NAME + '.pik'
 PICKLE_FILE2 = EXP_NAME2 + '.pik'
 
+USE_EXP1 = True
+USE_EXP2 = False
+
 RUN_PARSER = False
 RUN_PLOTTER = False
 
@@ -39,26 +42,28 @@ def parse_and_save(exp, data, pik):
         print('finished saving')
     return experiment
 
-try:
-    with open(PICKLE_FILE, 'rb') as f:
-        if RUN_PARSER:
-            experiment = parse_and_save(EXP_NAME, DATA_FOLDER, PICKLE_FILE)
-        else:
-            print('Opening file: ' + PICKLE_FILE)
-            experiment = pickle.load(f)
-except FileNotFoundError:
-    experiment = parse_and_save(EXP_NAME, DATA_FOLDER, PICKLE_FILE)
+if USE_EXP1:
+    try:
+        with open(PICKLE_FILE, 'rb') as f:
+            if RUN_PARSER:
+                experiment = parse_and_save(EXP_NAME, DATA_FOLDER, PICKLE_FILE)
+            else:
+                print('Opening file: ' + PICKLE_FILE)
+                experiment = pickle.load(f)
+    except FileNotFoundError:
+        experiment = parse_and_save(EXP_NAME, DATA_FOLDER, PICKLE_FILE)
 
 
-try:
-    with open(PICKLE_FILE2, 'rb') as f:
-        if RUN_PARSER:
-            experiment2 = parse_and_save(EXP_NAME2, DATA_FOLDER2, PICKLE_FILE2)
-        else:
-            print('Opening file: ' + PICKLE_FILE2)
-            experiment2 = pickle.load(f)
-except FileNotFoundError:
-    experiment2 = parse_and_save(EXP_NAME2, DATA_FOLDER2, PICKLE_FILE2)
+if USE_EXP2:
+    try:
+        with open(PICKLE_FILE2, 'rb') as f:
+            if RUN_PARSER:
+                experiment2 = parse_and_save(EXP_NAME2, DATA_FOLDER2, PICKLE_FILE2)
+            else:
+                print('Opening file: ' + PICKLE_FILE2)
+                experiment2 = pickle.load(f)
+    except FileNotFoundError:
+        experiment2 = parse_and_save(EXP_NAME2, DATA_FOLDER2, PICKLE_FILE2)
 
 
 if RUN_PLOTTER:
@@ -78,13 +83,16 @@ TRAIN_RATIO = 0.8
 IMG_DIM = 400
 
 trials = experiment.get_trials(list_type='all') #+ experiment2.get_trials(list_type='all')
+#trials = [trial for trial in trials if trial.stim_type_ind==2]
 
 if BINARY:
+    label_set = [0,1]
     if PIXEL_DATA:
-        X, Y = pixel_data.create_dataset(trials, binary=True)
+        X, Y = pixel_data.create_dataset(trials, label_set, binary=True)
     if HEATMAP_DATA:
         X, Y = heatmap_data.create_dataset(trials, binary=True)
     print(X.shape, Y.shape)
+    print("Dataset distribution:    Y==0: %.2f, Y==1: %.2f" % (len(Y[Y==0])/len(Y), len(Y[Y==1])/len(Y)))
     net = LeNet.LeNet_binary(LR=LR)
     print(net)
     print('BINARY')
